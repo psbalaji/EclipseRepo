b031e77ad6e6c04cd780837956eb74442985e98a
git <saibalaji.polakampally@accoliteindia.com>
2016-07-07 15:42:04 +0530

    Testing two java files both containing main class.

---
+++ src/com/accolite/corejava/FirstMainClass.java
@@ -0,0 +1,7 @@
+package com.accolite.corejava;
+
+public class FirstMainClass {
+	public static void main(String[] args) {
+		System.out.println("Hello world from fmc");
+	}
+}
---
+++ src/com/accolite/corejava/SecondMainClass.java
@@ -0,0 +1,8 @@
+package com.accolite.corejava;
+
+public class SecondMainClass {
+	public static void main(String[] args) {
+		FirstMainClass fMainClass = new FirstMainClass();
+		System.out.println("Hello world from smc");
+	}
+}
